0b750da00a5a392cb64f57cddba588371593e7e6
Gurkirat <grkit@Gurkirats-MacBook-Air.local>
2023-02-23 14:57:13 -0800

    first app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const style = {
+  width: "100%",
+  height: "100%",
+};
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Map
+          initialCenter={{
+            lat: 49.104600,
+            lng: -122.823510,
+          }}
+          google={this.props.google}
+          zoom={14}
+          containerStyle={style}
+        />
+        ;
+      </>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCIkm-WPSLmVV8b-gBDi94FD_rNC0HyfU4",
+})(App);
